9ed76e1c0f2b117aef4fab1b0b412ca34287e32d
Aurelie <albuzik97@gmail.com>
2021-08-05 20:59:04 +0700

    Шестая лекция

---
+++ src/main/java/Lesson6/socketExamples/ServerApp.java
@@ -0,0 +1,30 @@
+package Lesson6.socketExamples;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerApp {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(1234)) {
+            System.out.println("Сервер запущен, ожидаем соединения....");
+            Socket socket = serverSocket.accept();
+            System.out.println("Клиент подключился к серверу!");
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+
+            while (true) {
+                String clientRequest = dataInputStream.readUTF();
+                if (clientRequest.equals("end")) {
+                    break;
+                }
+                System.out.println("Мы получили строку: " + clientRequest);
+                dataOutputStream.writeUTF("Отвечаем на сообщение: " + clientRequest);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
